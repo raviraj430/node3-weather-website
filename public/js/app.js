1fb1d4f2a1cb26a36ca5ec98157fd37f8923e9ec
Ravi Raj <43921741+raviraj430@users.noreply.github.com>
2020-03-13 16:16:30 +0530

    some unecessary code is commented

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,9 @@
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
-    response.json().then((data) => {
-        console.log(data);
+// fetch('http://puzzle.mead.io/puzzle').then((response) => {
+//     response.json().then((data) => {
+//         console.log(data);
         
-  })
-})
-
-
+//   })
+// })
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
